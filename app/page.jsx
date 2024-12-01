4ff058e58b1c7c9b2fd722d4d6dffdc615d75e3c
Anne-Julie HOYÉ <94044617+annejuhoye@users.noreply.github.com>
2024-12-01 12:35:42 +0100

    add the page of Heartbit Project

--- app/page.jsx
+++ app/page.jsx
@@ -1,48 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
-//components
-import Socials from "@/components/Socials";
-import Photo from "@/components/Photo";
+"use client";
 
-
-const Home = () => {
+import Link from "next/link";
+const Heartbit = () => {
   return (
-    <section className="h-full">
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          
-          {/*text*/}
-          <div className="text-center xl:text-left order-2 xl:order-none">
-              <h1 className="h1 mb-8">
-                <span className="text-accent"> Anne-Julie HOYE</span>
-              </h1>
-              <h2 className="h3 mb-6"> Engineer Manager </h2>
-              <p className="max-w-[500px] mb-12 text-white/80"> Master's student in Creative Technology at ESILV. I create innovative projects as part of the Human Learning group.</p>
-              {/*btn and social*/}
-              <div className="flex flex-col xl:flex-row items-center gap-8">
-                <Button 
-                variant="outline" 
-                size="lg" 
-                className="uppercaseflex items-center gap-2"
-                >
-                  <a href="/assets/CV Anne-Julie HOYE.pdf" download>
-                    <span>Download CV</span>
-                  </a>
-                  <FiDownload className="text-xl" />
-                </Button>
-                <div className="mb-8 xl:mb-0">
-                  <Socials  containerStyles="flex gap-4" iconStyles="w-14 h-14 rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
-                </div>
-              </div>
-            </div>
-          
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
-          </div> 
-        </div>
+    <div ref={mountRef} className="relative w-full h-screen flex flex-col items-center justify-center">
+  
+      {/* Bouton "More details" */}
+      <div className="mt-4">
+        <Link href="https://ift.devinci.fr/member/anne-julie-hoye">
+          <button className="text-white underline font-semibold transition hover:text-blue-700 active:text-blue-900">
+            More details
+          </button>
+        </Link>
       </div>
-    </section>
+    </div>
   );
+  
 };
 
-export default Home;
+export default Heartbit;
